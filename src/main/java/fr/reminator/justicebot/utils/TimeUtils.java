41baec6b9042d2021b8b969a2fc0f561ee9ec52a
reminator <rlaborie2000@gmail.com>
2023-12-29 18:20:01 +0100

    Add TimeUtils

---
+++ src/main/java/fr/reminator/justicebot/utils/TimeUtils.java
@@ -0,0 +1,8 @@
+package fr.reminator.justicebot.utils;
+
+public class TimeUtils {
+
+    public static final long MS = 1;
+    public static final long SEC = 1000 * MS;
+    public static final long MIN = 60 * SEC;
+}
